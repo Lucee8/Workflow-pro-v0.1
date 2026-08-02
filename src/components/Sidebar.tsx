c695a86eb65c65b5751571b405d60995b90b8d22
Lucee8 <luceecode@gmail.com>
2026-08-02 23:40:27 +0530

    Updated project changes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -95,7 +95,7 @@ export default function Sidebar({
           <img 
             src={logoImg} 
             alt="Bhisez Logo" 
-            className="h-14 w-auto max-w-[160px] object-contain"
+            className="h-20 w-auto max-w-[180px] object-contain"
             referrerPolicy="no-referrer"
           />
         </div>
